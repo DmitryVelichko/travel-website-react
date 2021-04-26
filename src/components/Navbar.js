48ca472f73cdac50a451417c256493ab934bcf93
unknown <soleil-2005@mail.ru>
2021-04-26 13:20:09 +0300

    Finish Navbar.js, Add Button.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     const [click, setClick] = useState(false);
 
-     const handleClick = () => {
-         setClick(!click);
-    }
+    const handleClick = () => setClick(!click);
+    
+    const closeMobileMenu = () => setClick(false);
 
     return (
      <>
@@ -18,7 +18,28 @@ function Navbar() {
           <div className="menu-icon" onClick={handleClick}>
               <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
           </div>
-          <ul></ul> 
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}></ul> 
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+              </Link>
+            </li>
+             <li className='nav-item'>
+              <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+              Services
+              </Link>
+            </li>
+             <li className='nav-item'>
+              <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+              Products
+              </Link>
+            </li>
+             <li className='nav-item'>
+              <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+              Sign Up
+              </Link>
+            </li>
+            
         </div>
        </nav>
      </>
